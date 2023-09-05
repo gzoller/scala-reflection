e617d11819fea8d2604dbfaa78147154353e9880
Greg Zoller <gzoller@blocke.co>
2023-09-05 13:42:59 -0500

    3.3.0 support

--- src/main/scala/co.blocke.scala_reflection/ReflectionWorker.scala
+++ src/main/scala/co.blocke.scala_reflection/ReflectionWorker.scala
@@ -47,7 +47,7 @@ class ReflectionWorkerPhase extends PluginPhase {
       val reflected = RType.unwindType(quotes)(unpackedType,false)
       val s3ReflectionClassSymbol = getClassIfDefined("co.blocke.scala_reflection.S3Reflection")
       val annoArg = NamedArg("rtype".toTermName, Literal(quotes.reflect.StringConstant( reflected.serialize ).asInstanceOf[dotty.tools.dotc.core.Constants.Constant]))
-      tree.symbol.addAnnotation(Annotation.apply(s3ReflectionClassSymbol.asInstanceOf[ClassSymbol], annoArg) )
+      tree.symbol.addAnnotation(Annotation.apply(s3ReflectionClassSymbol.asInstanceOf[ClassSymbol], annoArg, tree.span) )
     tree
 }
 
